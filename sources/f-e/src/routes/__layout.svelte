918e708e18dc68253c28409a8bb3d7be8674ef5c
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2022-07-22 12:36:13 +0200

    added basic css styling

---
+++ sources/f-e/src/routes/__layout.svelte
@@ -0,0 +1,51 @@
+<script>
+  import '../global.css';
+  import Nav from '$lib/components/Nav/index.svelte';
+  import Footer from '$lib/components/Footer/index.svelte';
+</script>
+
+<style>
+  .main-container {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto 1fr auto;
+    grid-template-areas:
+      'nav-container'
+      'body-container'
+      'footer-container'
+    ;
+    gap: var(--main-gap);
+    height: 100vh;
+  }
+
+  .nav-container {
+    grid-area: nav-container;
+    display: flex;
+  }
+
+  .body-container {
+    grid-area: body-container;
+    display: flex;
+    flex-direction: column;
+    overflow: auto;
+
+    background-color: red;
+  }
+
+  .footer-container {
+    grid-area: footer-container;
+    display: flex;
+  }
+</style>
+
+<div class='main-container'>
+  <section class='nav-container'>
+    <Nav />
+  </section>
+  <main class='body-container'>
+    <slot></slot>
+  </main>
+  <section class='footer-container'>
+    <Footer />
+  </section>
+</div>
